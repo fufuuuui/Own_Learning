e2210a0d2074d1bb7fbfaca4c1c1646183adc1fe
fufuuuui <149380025+fufuuuui@users.noreply.github.com>
2025-11-08 17:10:33 +0900

    Add files via upload

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+// Your code goes here
+if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = +20;
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = +10;
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
+
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+
+Console.WriteLine(daysUntilExpiration);
