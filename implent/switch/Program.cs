61d9cbce07c705b4ac831ae611ae2346b3f33c51
fufuuuui <149380025+fufuuuui@users.noreply.github.com>
2025-11-12 15:53:37 +0900

    Add files via upload

--- implent/switch/Program.cs
+++ implent/switch/Program.cs
@@ -1,23 +1,100 @@
-﻿int employeeLevel = 100;
-string employeeName = "John Smith";
+﻿// int employeeLevel = 100;
+// string employeeName = "John Smith";
 
-string title = "";
+// string title = "";
 
-switch (employeeLevel)
+// switch (employeeLevel)
+// {
+//     case 100:
+//     case 200:
+//         title = "Senior Associate";
+//         break;
+//     case 300:
+//         title = "Manager";
+//         break;
+//     case 400:
+//         title = "Senior Manager";
+//         break;
+//     default:
+//         title = "Associate";
+//         break;
+// }
+
+// Console.WriteLine($"{employeeName}, {title}");
+
+string sku ="02-BL-M";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+switch (product[0])
 {
-    case 100:
-    case 200:
-        title = "Senior Associate";
-        break;
-    case 300:
-        title = "Manager";
-        break;
-    case 400:
-        title = "Senior Manager";
-        break;
+    case "01":
+        {
+            type = "Sweat shirt";
+            break;
+        }
+    case "02":
+        {
+            type = "T-shirt";
+            break;
+        }
+    case "03":
+        {
+            type = "Sweat pants";
+            break;
+        }
     default:
-        title = "Associate";
-        break;
+        {
+            type = "Other";
+            break;
+        }
 }
+switch (product[1])
+{
+    case "BL":
+        {
+            color = "Black";
+            break;
+        }
+    case "MN":
+        {
+            color = "Maroon";
+            break;
+        }
+    default:
+        {
+            color = "White";
+            break;
+        }
+}
+switch (product[2])
+{
+    case "S":
+        {
+            size = "Small";
+            break;
+        }
 
-Console.WriteLine($"{employeeName}, {title}");
+    case "M":
+        {
+            size = "Medium";
+            break;
+        }
+
+    case "L":
+        {
+            size = "Large";
+            break;
+        }
+
+    default:
+        {
+            size = "One Size Fits All";
+            break;
+        }
+}
+Console.WriteLine($"Product: {size} {color} {type}");
