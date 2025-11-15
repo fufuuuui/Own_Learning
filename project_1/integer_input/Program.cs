6ac1f75a48de68dff90fb0f13e81310da8470949
fufuuuui <149380025+fufuuuui@users.noreply.github.com>
2025-11-15 16:52:22 +0900

    Add files via upload

---
+++ project_1/integer_input/Program.cs
@@ -0,0 +1,23 @@
+﻿string? readResult;
+bool valid = false; 
+int numericValue = 0;
+
+Console.WriteLine("Enter an integer value between 5 and 10:");
+do
+{
+    readResult = Console.ReadLine(); 
+    if (int.TryParse(readResult, out numericValue))
+            if (numericValue > 10 || numericValue < 5)
+            {
+                Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
+            }
+            else if (numericValue >= 5 && numericValue <= 10)
+            {
+                valid = true;
+                Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you entered an invalid number, please try again");
+            }
+}while (valid == false);
