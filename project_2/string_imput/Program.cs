98fd6151a965b012fb597dc635d4fb85b054b087
fufuuuui <149380025+fufuuuui@users.noreply.github.com>
2025-11-18 21:46:16 +0900

    Add files via upload

---
+++ project_2/string_imput/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+
+string? readInput;
+bool success = true;
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+do
+{
+    readInput = Console.ReadLine().ToLower().Trim();
+    if (readInput != null)
+    {
+        if(readInput == "Administrator")
+        {
+            Console.WriteLine($"Your input value ({readInput}) has been accepted.");
+            success = false;
+        }
+        else if (readInput == "Manager")
+        {
+            Console.WriteLine($"Your input value ({readInput}) has been accepted.");
+            success = false;
+        }
+        else
+        {
+            Console.WriteLine($"The role name that you entered, \"{readInput}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+            success = false;
+        }
+    }
+}while(success);
