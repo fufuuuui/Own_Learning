768741f5649b91b28a3d64d3566b2af88bfcb882
fufuuuui <149380025+fufuuuui@users.noreply.github.com>
2025-11-19 14:58:11 +0900

    Add files via upload

---
+++ project_3/string_arr/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+
+string[] periodLocation  = myStrings;
+
+foreach (var item in periodLocation)
+{
+    
+    int index = item.IndexOf(".");
+    do
+    {
+        Console.WriteLine(item + "\n");
+    }while (item.IndexOf(".") + 1 == index + 1);
+}
