5e0c160cd20e3da29f6cda8eb8b528767e80e1f9
fufuuuui <149380025+fufuuuui@users.noreply.github.com>
2025-11-19 22:50:23 +0900

    Add files via upload

---
+++ integer_input/Program.cs
@@ -0,0 +1,60 @@
+﻿string? readResult;
+bool valid = false; 
+int numericValue = 0;
+
+Console.WriteLine("Enter an integer value between 5 and 10:");
+do
+{
+    readResult = Console.ReadLine(); 
+    if (int.TryParse(readResult, out numericValue))
+            if (numericValue > 10 || numericValue < 5)
+            {
+                Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
+            }
+            else if (numericValue >= 5 && numericValue <= 10)
+            {
+                valid = true;
+                Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you entered an invalid number, please try again");
+            }
+}while (valid == false);
+
+// Better Code
+
+// string? readResult;
+// string valueEntered = "";
+// int numValue = 0;
+// bool validNumber = false;
+
+// Console.WriteLine("Enter an integer value between 5 and 10");
+
+// do
+// {
+//     readResult = Console.ReadLine();
+//     if (readResult != null) 
+//     {
+//         valueEntered = readResult;
+//     }
+
+//     validNumber = int.TryParse(valueEntered, out numValue);
+
+//     if (validNumber == true)
+//     {
+//         if (numValue <= 5 || numValue >= 10)
+//         {
+//             validNumber = false;
+//             Console.WriteLine($"You entered {numValue}. Please enter a number between 5 and 10.");
+//         }
+//     }
+//     else 
+//     {
+//         Console.WriteLine("Sorry, you entered an invalid number, please try again");
+//     }
+// } while (validNumber == false);
+
+// Console.WriteLine($"Your input value ({numValue}) has been accepted.");
+
+// readResult = Console.ReadLine();
---
+++ string_imput/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+
+string? readInput;
+bool success = true;
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+do
+{
+    readInput = Console.ReadLine().Trim();
+    if (readInput != null)
+    {
+        if(readInput.ToLower() == "administrator")
+        {
+            Console.WriteLine($"Your input value ({readInput}) has been accepted.");                        
+            success = false;
+        }
+        else if(readInput.ToLower() == "manager")
+        {
+            Console.WriteLine($"Your input value ({readInput}) has been accepted.");
+            success = false;
+        }
+        else if(readInput.ToLower() == "user")
+        {
+            Console.WriteLine($"Your input value ({readInput}) has been accepted.");
+            success = false;
+        }
+        
+        else
+        {
+            Console.WriteLine($"The role name that you entered, \"{readInput}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+            success = false;
+        }
+    }
+}while(success);
+
+// Better Code
+
+// string? readResult;
+// string roleName = "";
+// bool validEntry = false;
+
+// do
+// {                
+//     Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+//     readResult = Console.ReadLine();
+//     if (readResult != null) 
+//     {
+//         roleName = readResult.Trim();
+//     }
+
+//     if (roleName.ToLower() == "administrator" || roleName.ToLower() == "manager" || roleName.ToLower() == "user") 
+//     {
+//         validEntry = true;
+//     }
+//     else
+//     {
+//         Console.Write($"The role name that you entered, \"{roleName}\" is not valid. ");
+//     }
+
+// } while (validEntry == false);
+
+// Console.WriteLine($"Your input value ({roleName}) has been accepted.");
+// readResult = Console.ReadLine();
