2a2f1e29abcce590dc713bc467211e5bfd46a5c3
fufuuuui <149380025+fufuuuui@users.noreply.github.com>
2025-12-01 18:22:16 +0800

    Add files via upload

---
+++ com_Strarr_int/chna/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+
+decimal results = 0;
+string letter = "";
+
+for (int i = 0; i < values.Length; i++)
+{
+    decimal result = 0;
+    if (decimal.TryParse(o, out result))
+    {   
+        results += result;
+    }
+    else
+    {
+        letter += values[i];
+    }
+
+}
+Console.WriteLine($"Message: {letter}");
+Console.WriteLine($"Total: {results}");
