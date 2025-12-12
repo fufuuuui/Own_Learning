d0d0e5e871727c9d9f3cdd3ffaabb529b33a2afe
fufuuuui <149380025+fufuuuui@users.noreply.github.com>
2025-12-12 22:43:22 +0800

    Add files via upload

---
+++ reverse_words/Program.cs
@@ -0,0 +1,12 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+foreach (var pangramArray in pangram.Split(" ", 1))
+{
+    Console.WriteLine(pangramArray);
+    if (pangramArray.Length > 0)
+    {
+        char[] raversed = pangramArray.ToCharArray();
+        Array.Reverse(raversed);
+        string result = string.Join("", raversed);
+        Console.WriteLine(result);
+    }
+}
