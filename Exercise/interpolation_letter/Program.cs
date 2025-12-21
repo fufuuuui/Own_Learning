7d00e0a4a15c1479965886ddd57a8357947fd5e2
[fufuu watson] <fufuuwatson@gmail.com>
2025-12-21 16:36:12 +0800

    w

--- Exercise/interpolation_letter/Program.cs
+++ Exercise/interpolation_letter/Program.cs
@@ -1,4 +1,42 @@
-﻿string customerName = "Ms. Barros";
+﻿// string customerName = "Ms. Barros";
+
+// string currentProduct = "Magic Yield";
+// int currentShares = 2975000;
+// decimal currentReturn = 0.1275m;
+// decimal currentProfit = 55000000.0m;
+
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
+
+// // Your logic here
+
+// var name = customerName + ",";
+// var greeting = $"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.";
+// var body = $"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}%.";
+// var closing = $"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be ${newProfit:N2}";
+
+// var magic_Yield = currentProduct.PadRight(10);
+// magic_Yield += currentReturn.ToString("P2").PadLeft(14);
+// magic_Yield += currentProfit.ToString("N2").PadLeft(20);
+
+// var glorious_Future = newProduct.PadRight(10);
+// glorious_Future += newReturn.ToString("P2").PadLeft(10);
+// glorious_Future += newProfit.ToString("N2").PadLeft(20);
+// // Your logic here
+// Console.WriteLine(name + "\n");
+// Console.WriteLine(greeting + "\n");
+// Console.WriteLine(body + "\n");
+// Console.WriteLine(closing + "\n");
+
+// Console.WriteLine("Here's a quick comparison:" + "\n");
+
+// Console.WriteLine(magic_Yield);
+// Console.WriteLine(glorious_Future);
+
+// Better Solution
+
+string customerName = "Ms. Barros";
 
 string currentProduct = "Magic Yield";
 int currentShares = 2975000;
@@ -9,27 +47,22 @@ string newProduct = "Glorious Future";
 decimal newReturn = 0.13125m;
 decimal newProfit = 63000000.0m;
 
-// Your logic here
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
 
-var name = customerName + ",";
-var greeting = $"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.";
-var body = $"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}%.";
-var closing = $"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be ${newProfit:N2}";
+Console.WriteLine("Here's a quick comparison:\n");
 
-var magic_Yield = currentProduct.PadRight(10);
-magic_Yield += currentReturn.ToString("P2").PadLeft(14);
-magic_Yield += currentProfit.ToString("N2").PadLeft(20);
+string comparisonMessage = "";
 
-var glorious_Future = newProduct.PadRight(10);
-glorious_Future += newReturn.ToString("P2").PadLeft(10);
-glorious_Future += newProfit.ToString("N2").PadLeft(20);
-// Your logic here
-Console.WriteLine(name + "\n");
-Console.WriteLine(greeting + "\n");
-Console.WriteLine(body + "\n");
-Console.WriteLine(closing + "\n");
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
 
-Console.WriteLine("Here's a quick comparison:" + "\n");
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
 
-Console.WriteLine(magic_Yield);
-Console.WriteLine(glorious_Future);
+Console.WriteLine(comparisonMessage);
