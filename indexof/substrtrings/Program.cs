64e426ff3ddf65ca7a314d4ed51f9b393e08be29
[fufuu watson] <fufuuwatson@gmail.com>
2025-12-22 17:10:24 +0800

    w

---
+++ indexof/substrtrings/Program.cs
@@ -0,0 +1,15 @@
+﻿string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+Console.WriteLine(openingPosition);
+Console.WriteLine(closingPosition);
+
+openingPosition += openSpan.Length; // Move to the first character inside the parentheses
+
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
