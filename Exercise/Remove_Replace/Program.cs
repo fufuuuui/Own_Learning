e28cc8b7ccbc50be53026f8c2f27380a91a02424
[fufuu watson] <fufuuwatson@gmail.com>
2025-12-27 17:24:44 +0800

    w

--- Exercise/Remove_Replace/Program.cs
+++ Exercise/Remove_Replace/Program.cs
@@ -7,15 +7,51 @@
 // message = message.Replace("-", "");
 // Console.WriteLine(message);
 
+// const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+// string quantity = "";
+// string output = "";
+
+// // Your work here
+// quantity = input.Remove(input.IndexOf("<div>"), input.IndexOf("5000"));
+// output = input.Replace("</div>", "")
+//     .Replace("<div>", "");
+
+// Console.WriteLine("Quantity: " + quantity.Remove(4, 13));
+// Console.WriteLine("Output: " + output);
+
+// Better Solutuion:
 const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
 string quantity = "";
 string output = "";
 
 // Your work here
-quantity = input.Remove(input.IndexOf("<div>"), input.IndexOf("5000"));
-output = input.Replace("</div>", "")
-    .Replace("<div>", "");
 
-Console.WriteLine("Quantity: " + quantity.Remove(4, 13));
-Console.WriteLine("Output: " + output);
+// Extract the "quantity"
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int quantityStart = input.IndexOf(openSpan) + openSpan.Length; // + length of <span> so index at end of <span> tag
+int quantityEnd= input.IndexOf(closeSpan);
+int quantityLength = quantityEnd - quantityStart;
+quantity = input.Substring(quantityStart, quantityLength);
+quantity = $"Quantity: {quantity}";
+
+// Set output to input, replacing the trademark symbol with the registered trademark symbol
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+output = input.Replace(tradeSymbol, regSymbol);
+
+// Remove the opening <div> tag
+const string openDiv = "<div>";
+int divStart = output.IndexOf(openDiv);
+output = output.Remove(divStart, openDiv.Length);
+
+// Remove the closing </div> tag and add "Output:" to the beginning
+const string closeDiv = "</div>";
+int divCloseStart = output.IndexOf(closeDiv);
+output = "Output: " + output.Remove(divCloseStart, closeDiv.Length);
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
