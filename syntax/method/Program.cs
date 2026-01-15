2be5fe45925057bbdd0e20d093db4d2cd6022d31
[fufuu watson] <fufuuwatson@gmail.com>
2026-01-15 17:18:10 +0800

    w

--- syntax/method/Program.cs
+++ syntax/method/Program.cs
@@ -24,13 +24,16 @@
 // void ShowData(string a, int b, int c);
 // void DisplayDate(string month, int day, int year);
 
+Console.WriteLine("Generating random numbers:");
+DisplayRandomNumbers();
+
 void DisplayRandomNumbers()
 {
     Random random = new Random();
 
     for (int i = 0; i < 5; i++)
     {
-        Console.Write($"{random.Next(1, 100)} ");
+        Console.WriteLine($"{random.Next(1, 100)} ");
     }
     Console.WriteLine();
 }
