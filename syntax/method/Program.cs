68378e67c29e81a289a433ee93cd6e9e6ba87869
[fufuu watson] <fufuuwatson@gmail.com>
2026-01-15 17:10:12 +0800

    w

---
+++ syntax/method/Program.cs
@@ -0,0 +1,25 @@
+﻿// // int[] a = {1,2,3,4,5};
+
+// // Console.WriteLine("Contents of Array:");
+// // PrintArray();
+
+// // void PrintArray()
+// // {
+// //     foreach (int x in a)
+// //     {
+// //         Console.Write($"{x}");
+// //     }
+// //     Console.WriteLine();
+// // }
+
+// Console.WriteLine("Before calling a method");
+// SayHello();
+// Console.WriteLine("After calling a method");
+
+// void SayHello() 
+// {
+//     Console.WriteLine("Hello World!");
+// }
+
+void ShowData(string a, int b, int c);
+void DisplayDate(string month, int day, int year);
